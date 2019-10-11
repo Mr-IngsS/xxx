040230b62c01a42607a23686ec679af06d6e0463
Mr-IngsS <54253110+Mr-IngsS@users.noreply.github.com>
2019-10-11 07:58:46 +0700

    XxxBCT

---
+++ Toolsxxx.sh
@@ -0,0 +1,156 @@
+#!/system/xbin/bash/
+clear
+toilet -f slant --gay "hello"
+echo "\033[32;1m|=======================================|"
+echo "\033[32;1m|   •   Author  : Mr.IngsS        •     |"
+echo "\033[34;1m|   •   Tools   : Installer       •     |"
+echo "\033[32;1m|   •   My Team : BlackCyberTeam  •     |"
+echo "\033[34;1m|   •   Massage : nothing is safe •     |"
+echo "\033[32;1m|=======================================|"
+echo "\033[36;1m Welcome To Tools BlackCyberTeam"
+echo "\033[36;1m Silahkan Pilih Tools >>>"
+echo "\033[32;1m|=============MENU TOOLS===========|q|"
+echo "\033[32;1m|   Δ   [1].SPAM CALL >>           |"
+echo "\033[32;1m|   Δ   [2].FACEBOOK HACKED >>     |"
+echo "\033[32;1m|   Δ   [3].DARK-SMS >>            |"
+echo "\033[32;1m|   Δ   [4].BRUTEFORCE FACEBOOK >> |"
+echo "\033[32;1m|   Δ   [5].BRUTEFORCE GMAIL >>    |"
+echo "\033[32;1m|   Δ   [6].INFO TEAM >>           |"
+echo "\033[32;1m|   Δ   [7].RED-HAWK >>            |"
+echo "\033[32;1m|   Δ   [8].HACKED INSTAGRAM >>    |"
+echo "\033[32;1m|   Δ   [9].CREATE DEFACE >>       |"
+echo "\033[32;1m|   Δ   [10].EXIT/CLOSE >>         |"
+echo "\033[32;1m|==================================|"
+read -p "[=====>" rgl
+
+if [$rgl = 1 ] || [ $rgl = 1 ]
+then
+clear
+toilet -f slant --gay "Welcome"
+apt update && apt upgrade
+apt install php
+apt install git
+git clone https://github.com/4L13199/LITESPAM.git
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [$rgl = 2 ] || [ $rgl = 2 ]
+then
+clear
+toilet -f slant --gay "welcome"
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/bukanhacker/Sad.git
+cd Sad
+python2 Sad.py
+fi
+
+if [$rgl = 3 ] || [ $rgl = 3 ]
+then
+clear
+toilet -f slant --gay "welcome"
+apt update && apt upgrade
+pkg install python2
+pkg install pip
+pkg install pip2
+pip 2 install requests
+pkg install git
+git clone https://github.com/xractz/fsms.git
+cd fsms
+python fsms.py
+fi
+
+if [ $rgl = 4 ] || [ $rgl = 4 ]
+then
+clear
+toilet -f slant --gay "welcome"
+echo "\033[36;1m|======================================================|"
+echo "\033[36;1m|   •   Silahkan Hubungi Kami Jika Ingin Script.  •    |"
+echo "\033[36;1m|   •   Contact WA : 089512758056 [Mr.IngsS].     •    |"
+echo "\033[36;1m|   •   Contact Email : Wahyuncax@gmail.com.      •    |"
+echo "\033[36;1m|   •   Contact Wa : 089632621709 [Mr.H4CKYOU7H]. •    |"
+echo "\033[36;1m|   •   Contact Email : ariomuhammad55@gmail.com. •    |"
+echo "\033[36;1m|======================================================|"
+fi
+
+if [$rgl = 5 ] || [ $rgl = 5 ]
+then
+clear
+toilet -f slant --gay "welcome"
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/Ha3MrX/Gemail-Hack.git
+cd Gemail-Hack
+python2 gemailhack.py
+fi
+
+if [$rgl = 6 ] || [ $rgl = 6 ]
+then
+clear
+toilet -f slant --gay "<|=== Me ==|>"
+sleep 1
+echo "\033[32;1m|===============================|"
+echo "\033[31;1m|   •   I am Mr.IngsS        •  |"
+echo "\033[32;1m|  •    I am Mr.H4CKYOU7H_  •   |"
+echo "\033[31;1m|   •   I am Mr.ClayBot      •  |"
+echo "\033[32;1m|  •    I am Mr.Style       •   |"
+echo "\033[31;1m|   •   I am Mr.Codex        •  |"
+echo "\033[32;1m|  •    I am Mr.JanCok      •   |"
+echo "\033[31;1m|   •   I am TciD.MitnicK    •  |"
+echo "\033[32;1m|===============================|"
+fi
+
+if [$rgl = 7 ] || [ $rgl = 7 ]
+then
+clear
+toilet -f slant --gay "welcome"
+pkg update && pkg upgrade
+pkg install git
+pkg install pytho2
+pkg install php
+git clone https://github.com/Tuhinshubhra/RED_HAWK.git
+cd RED-HAWK
+chmod +x rhawk.php
+php rhwak.php
+fi
+
+if [$rgl = 8 ] || [ $rgl = 8 ]
+then
+clear
+toilet -f slant --gay "welcome"
+echo "\033[36;1m|======================================================|"
+echo "\033[36;1m|   •   Silahkan Hubungi Kami Jika Ingin Script.  •    |"
+echo "\033[36;1m|   •   Contact WA : 089512758056 [Mr.IngsS].     •    |"
+echo "\033[36;1m|   •   Contact Email : Wahyuncax@gmail.com.      •    |"
+echo "\033[36;1m|   •   Contact Wa : 089632621709 [Mr.H4CKYOU7H]. •    |"
+echo "\033[36;1m|   •   Contact Email : ariomuhammad55@gmail.com. •    |"
+echo "\033[36;1m|======================================================|"
+fi
+
+if [$rgl = 9 ] || [ $rgl = 9 ]
+then
+clear
+toilet -f slant --gay "welcome"
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/4L13199/LITESCRIPT.git
+cd LITESCRIPT
+chmod 777 LITESCRIPT.py
+python2 LITESCRIPT.sh
+fi
+
+if [ $rgl = 10 ] || [ $rgl = 10 ]
+then
+clear
+toilet -f slant --gay "EXIT"
+echo "\033[36;1m|=======================================================|"
+echo "\033[36;1m|   *   Terima Kasih Sudah Menggunakan Tools Kami       |"
+echo "\033[36;1m|   *   Lakukan Lah Selagi Kalian Bisa                  |"
+echo "\033[36;1m|   *   Thanks To : BlackCyberTeam                      |"
+echo "\033[36;1m|=======================================================|"
+exit
+fi
